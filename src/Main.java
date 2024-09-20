82c772d47b655cb72b81ee74bb66c2a462208afb
91901 <koumika.chitti1016@gmail.com>
2024-09-21 00:06:44 +0530

    21092024

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+
+    public static void main(String[] args) {
+
+        int first = 30;
+        int second = 70;
+
+        // add two numbers
+        int sum = first + second;
+        System.out.println(first + " + " + second + " = "  + sum);
+    }
+}
---
+++ src/lab.java
@@ -0,0 +1,9 @@
+class lab {
+    public static void main(String[] args) {
+        int a = 60;
+        int b= 20;
+        // add two numbers
+        int c = a+b;
+        System.out.println(c);
+    }
+}
